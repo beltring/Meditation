5690972589070268cb4a81c2a2edf299a66e5d6c
Pavel Boltromyuk <pavel.boltromyuk99@gmail.com>
2021-09-20 09:36:43 +0300

    added extensions

---
+++ Meditation/Meditation/Modules/Root/RootNavigationViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RootNavigationViewController.swift
+//  Meditation
+//
+//  Created by User on 20.09.21.
+//
+
+import UIKit
+
+class RootNavigationViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func setRootController() {
+        let vc: UIViewController
+    }
+}
