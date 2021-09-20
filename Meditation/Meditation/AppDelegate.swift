19882f59d87129e17c6c539fbae5b23fc38702f6
Pavel Boltromyuk <pavel.boltromyuk99@gmail.com>
2021-09-20 09:07:56 +0300

    connect firebase

--- Meditation/Meditation/AppDelegate.swift
+++ Meditation/Meditation/AppDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by User on 20.09.21.
 //
 
+import Firebase
 import UIKit
 
 @main
@@ -13,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 }
