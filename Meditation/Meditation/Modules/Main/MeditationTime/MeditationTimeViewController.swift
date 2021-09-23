97f0502aa40c505e6915f527af4f049391677ef4
Pavel Boltromyuk <pavel.boltromyuk99@gmail.com>
2021-09-23 14:42:40 +0300

    added date picker

--- Meditation/Meditation/Modules/Main/MeditationTime/MeditationTimeViewController.swift
+++ Meditation/Meditation/Modules/Main/MeditationTime/MeditationTimeViewController.swift
@@ -12,7 +12,10 @@ import UIKit
 class MeditationTimeViewController: UIViewController {
     
     @IBOutlet weak var startNowButton: UIButton!
+    @IBOutlet weak var timeLabel: UILabel!
     
+    var toolBar = UIToolbar()
+    var datePicker  = UIDatePicker()
     let user = Auth.auth().currentUser
     
     // MARK: - Lifecycle
@@ -20,6 +23,7 @@ class MeditationTimeViewController: UIViewController {
         super.viewDidLoad()
         setupButton()
         setupNavBar()
+        setupLabel()
     }
     
     // MARK: - Setup
@@ -45,8 +49,47 @@ class MeditationTimeViewController: UIViewController {
         navigationController?.navigationBar.topItem?.titleView = imageView
     }
     
+    private func setupLabel() {
+        timeLabel.isUserInteractionEnabled = true
+        let labelTapGesture = UITapGestureRecognizer(target:self,action:#selector(self.timeLabelTap))
+        timeLabel.addGestureRecognizer(labelTapGesture)
+    }
+    
     // MARK: - Actions
     @IBAction func tappedStartNow(_ sender: UIButton) {
         print("Tapped start")
+    }
+    
+    @objc private func timeLabelTap() {
+        datePicker = UIDatePicker.init()
+        datePicker.backgroundColor = UIColor(named: "BackgroundColor")
+                
+        datePicker.autoresizingMask = .flexibleWidth
+        datePicker.datePickerMode = .countDownTimer
+        datePicker.frame = CGRect(x: 0.0, y: UIScreen.main.bounds.size.height - 300, width: UIScreen.main.bounds.size.width, height: 300)
+        self.view.addSubview(datePicker)
+                
+        let buttonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.onDoneButtonClick))
+        buttonItem.tintColor = .white
+        toolBar = UIToolbar(frame: CGRect(x: 0, y: UIScreen.main.bounds.size.height - 300, width: UIScreen.main.bounds.size.width, height: 50))
+        toolBar.barTintColor = UIColor(named: "BackgroundColor")
+        toolBar.barStyle = .default
+        toolBar.items = [UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil), buttonItem]
+        toolBar.sizeToFit()
+        self.view.addSubview(toolBar)
+    }
+
+    @objc func onDoneButtonClick() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm:ss"
+            
+        var time = dateFormatter.string(from: datePicker.date)
+        let hours = time.prefix(3)
+        if hours == "00:" {
+            time = time.replacingOccurrences(of: "00:", with: "")
+        }
+        timeLabel.text = time
+        toolBar.removeFromSuperview()
+        datePicker.removeFromSuperview()
     }
 }
