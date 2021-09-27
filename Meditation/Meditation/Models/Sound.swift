c9e1010897f864efee11fa9f1acf89a6af3f7a81
Pavel Boltromyuk <pavel.boltromyuk99@gmail.com>
2021-09-27 16:19:54 +0300

    add sound model

---
+++ Meditation/Meditation/Models/Sound.swift
@@ -0,0 +1,16 @@
+//
+//  Sound.swift
+//  Meditation
+//
+//  Created by Pavel Boltromyuk on 27.09.21.
+//
+
+import Foundation
+import UIKit
+
+struct Sound {
+    let title: String
+    let image: UIImage?
+    let countListening: Int
+    let duration: String
+}
