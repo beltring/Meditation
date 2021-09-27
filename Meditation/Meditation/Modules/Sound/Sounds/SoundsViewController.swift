106d0a3c844a7fb726c17f538bd1e68e712fadb3
Pavel Boltromyuk <pavel.boltromyuk99@gmail.com>
2021-09-27 14:27:24 +0300

    added table view for sounds

--- Meditation/Meditation/Modules/Sound/Sounds/SoundsViewController.swift
+++ Meditation/Meditation/Modules/Sound/Sounds/SoundsViewController.swift
@@ -9,10 +9,42 @@ import UIKit
 
 class SoundsViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        SoundTableViewCell.registerCellNib(in: tableView)
     }
 
+}
+
+// MARK: - Extensions
+// MARK: - UITableViewDataSource
+extension SoundsViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = SoundTableViewCell.dequeueReusableCell(in: tableView, for: indexPath)
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension SoundsViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        headerView.backgroundColor = UIColor(named: "BackgroundColor")
+        return headerView
+    }
 }
