68d868bb460d2239530b5bcdd0adb1a326d19457
Pavel Boltromyuk <pavel.boltromyuk99@gmail.com>
2021-10-15 15:18:06 +0300

    added events

---
+++ Meditation/Meditation/Helpers/Analytics/Event.swift
@@ -0,0 +1,30 @@
+//
+//  Event.swift
+//  Meditation
+//
+//  Created by Pavel Boltromyuk on 15.10.21.
+//
+
+import Foundation
+
+enum Event: String {
+    case loginWithEmailTapped
+    case loginTapped
+    case forgotPasswordTapped
+    case tappedStartNow
+    case choiceTime
+    case choiceCalmMeditation
+    case choiceRelaxMeditation
+    case choiceFocusMeditation
+    case choiceAnxiousMeditation
+    case playMusic
+    case playNow
+    case nextMusic
+    case prevMusic
+    case repeatMusic
+    case shuffleMusic
+    case stopMusic
+    case editScreen
+    case editTapped
+    case choiceImage
+}
